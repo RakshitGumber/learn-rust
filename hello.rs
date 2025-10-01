cdf90e25bd25bb3d5b4e2302a5b27e8abc3f8915
RakshitGumber <gumberrakshit@gmail.com>
2025-10-02 01:33:51 +0530

    Hello World

--- hello.rs
+++ hello.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, World!")
+    println!("Hello, World!\nI'm a Rustacean");
+    println!("{number:->width$}", number=2, width=5)
 }
+
+// https://doc.rust-lang.org/rust-by-example/
