235ea9ff043ee43df8cadcb1cd6a30526725733a
RakshitGumber <gumberrakshit@gmail.com>
2025-10-02 09:27:02 +0530

    Learning rust formatting

--- hello.rs
+++ hello.rs
@@ -1,6 +1,16 @@
 fn main() {
-    println!("Hello, World!\nI'm a Rustacean");
-    println!("{number:->width$}", number=2, width=5)
+    // println!("Hello, World!\nI'm a Rustacean");
+    // println!("{number:->width$}", number=2, width=5);
+
+    // println!("Base 10:{:>20}",   69420); 
+    // println!("Base 2 (binary):       {:b}", 69420);
+    // println!("Base 8 (octal):        {:o}", 69420); 
+    // println!("Base 16 (hexadecimal): {:x}", 69420); 
+
+    // println!("My name is {0}, {1} {0}", "Bond");
+
+    
+
 }
 
 // https://doc.rust-lang.org/rust-by-example/
